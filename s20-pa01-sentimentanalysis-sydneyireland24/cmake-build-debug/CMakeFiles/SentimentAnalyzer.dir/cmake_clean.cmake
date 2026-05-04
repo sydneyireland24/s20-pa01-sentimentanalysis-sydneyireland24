4e24552853fc4e1c3b5c6ef6f492e8484bcd8cae
Sydney Gibbs <54719754+sydneyireland24@users.noreply.github.com>
2026-05-03 21:19:16 -0500

    Add files via upload

---
+++ s20-pa01-sentimentanalysis-sydneyireland24/cmake-build-debug/CMakeFiles/SentimentAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SentimentAnalyzer.dir/dsstring.cpp.obj"
+  "CMakeFiles/SentimentAnalyzer.dir/main.cpp.obj"
+  "CMakeFiles/SentimentAnalyzer.dir/test.cpp.obj"
+  "SentimentAnalyzer.exe"
+  "SentimentAnalyzer.exe.manifest"
+  "SentimentAnalyzer.pdb"
+  "libSentimentAnalyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SentimentAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ s20-pa01-sentimentanalysis-sydneyireland24/cmake-build-debug/CMakeFiles/SentimentAnalyzer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SentimentAnalyzer.
+# This may be replaced when dependencies are built.
